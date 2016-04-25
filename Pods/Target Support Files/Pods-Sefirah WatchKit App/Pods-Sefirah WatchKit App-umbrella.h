bf48c034aa9103165ca82029edec0cd155e35bdf
SteenJobs <josh.e.siegel@gmail.com>
2016-04-25 16:56:41 -0400

    Removed all tracked files added to .gitignore

--- Pods/Target Support Files/Pods-Sefirah WatchKit App/Pods-Sefirah WatchKit App-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-
-FOUNDATION_EXPORT double Pods_Sefirah_WatchKit_AppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Sefirah_WatchKit_AppVersionString[];
-
